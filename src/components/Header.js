4dc8a115ed042321feea7adb08ff1b80abc01e86
Luis Zubia <soyzubia@gmail.com>
2023-03-28 09:17:41 -0700

    Add inline styles with an initilized style object

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
-const Header = () => (
-  <header style={{
+const Header = () => {
+  const headerStyle = {
     padding: '20px 0',
     lineHeight: '1.5em',
     color: '#aeadad',
     textAlign: 'center',
-  }}
-  >
-    <h1>todos</h1>
-    <p>Items will persist in the browser local storage</p>
-  </header>
-);
+  };
+
+  return (
+    <header style={headerStyle}>
+      <h1>todos</h1>
+      <p>Items will persist in the browser local storage</p>
+    </header>
+  );
+};
 
 export default Header;
