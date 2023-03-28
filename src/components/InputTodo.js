8ec41bf0e564552783620b1976dab02f5ea170e9
Luis Zubia <soyzubia@gmail.com>
2023-03-27 23:13:34 -0700

    Add functionality for display a message when user tries to submit an empty new Todo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     setTitle(e.target.value);
@@ -14,21 +15,25 @@ const InputTodo = ({ addTodoItem }) => {
     if (title.trim()) {
       addTodoItem(title);
       setTitle('');
+      setMessage('');
     } else {
-      alert('Please add item');
+      setMessage('Please add item.');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add Todo..."
-        value={title}
-        onChange={handleChange}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <span>{message}</span>
+    </>
   );
 };
 
