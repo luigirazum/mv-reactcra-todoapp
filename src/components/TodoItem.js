159e1ca20cb11f6bf3620b593df22bda82c950f5
Luis Zubia <soyzubia@gmail.com>
2023-03-27 19:16:32 -0700

    Add props validation for TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,3 +1,11 @@
+import PropTypes from 'prop-types';
+
 const TodoItem = ({ itemProp }) => <li>{itemProp.title}</li>;
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default TodoItem;
