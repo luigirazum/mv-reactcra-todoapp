03d6b10e824dd40d4ec9a5074ddbdea45ee7441c
Luis Zubia <soyzubia@gmail.com>
2023-03-27 21:27:00 -0700

    INI #raising-handling-events && Add input checkbox to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types';
 
-const TodoItem = ({ itemProp }) => <li>{itemProp.title}</li>;
+const TodoItem = ({ itemProp }) => (
+  <li>
+    {/* INI #raising-handling-events */}
+    <input type="checkbox" />
+    {itemProp.title}
+  </li>
+);
 
 TodoItem.propTypes = {
   itemProp: PropTypes.shape({
