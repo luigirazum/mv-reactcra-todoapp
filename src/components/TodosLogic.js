95a48ad2f5ebceb2c4243a06f705f9b3a310065d
Luis Zubia <soyzubia@gmail.com>
2023-03-27 18:38:17 -0700

    Render a list of todos in TodosLogic component

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,5 +1,29 @@
-const TodosLogic = () => (
-  <div>TodosLogic content</div>
-);
+const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default TodosLogic;
