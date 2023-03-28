f900b2d309f17511e87fbccb13cc3e811b2ee57c
Luis Zubia <soyzubia@gmail.com>
2023-03-27 21:57:08 -0700

    Add handleChange to TodoItem with setTodos updater in a controled input checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,36 @@
 import PropTypes from 'prop-types';
 
-const TodoItem = ({ itemProp, setTodos }) => (
-  <li>
-    <input type="checkbox" />
-    {itemProp.title}
-  </li>
-);
+const TodoItem = ({ itemProp, setTodos }) => {
+  const handleChange = (id) => {
+    setTodos((prevState) => prevState
+      .map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }));
+  };
+
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      {itemProp.title}
+    </li>
+  );
+};
 
 TodoItem.propTypes = {
   itemProp: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
   }).isRequired,
   setTodos: PropTypes.func.isRequired,
 };
