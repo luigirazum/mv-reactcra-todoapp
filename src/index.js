18f16ff6eb09d6457f83da39eb7968a28871daf3
Luis Zubia <soyzubia@gmail.com>
2023-03-27 17:39:35 -0700

    Use absolute imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-
-import TodoApp from './components/TodoApp';
+import TodoApp from 'components/TodoApp';
+import reportWebVitals from 'reportWebVitals';
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
