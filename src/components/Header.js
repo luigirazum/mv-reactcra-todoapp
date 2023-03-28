7ddc6183232bb830265f6363b4da2c06cf9f3339
Luis Zubia <soyzubia@gmail.com>
2023-03-28 09:14:42 -0700

    Add inline styles to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 const Header = () => (
-  <header>
+  <header style={{
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  }}
+  >
     <h1>todos</h1>
     <p>Items will persist in the browser local storage</p>
   </header>
